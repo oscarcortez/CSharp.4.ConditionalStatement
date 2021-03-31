e6a8eec98dcf33a66ce951a70fe6134d5cbd674e
oscarcortez <oscarkortez@gmail.com>
2021-03-31 18:04:08 -0400

    Add project files.

---
+++ ConsoleApp4IF/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+            else if(a +b < 10)
+            {
+                Console.WriteLine("The answer is not greater than 10.");
+            }
+            else if(a + b == 0)
+            {
+                Console.WriteLine("the answer is equal to zero");
+            }
+            Console.WriteLine(a + b > 10 ? "es mayor a 10" : "no es mayor a 10");
+        }
+    }
+}
